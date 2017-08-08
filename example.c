48d67bac06dc5eeaa8488cae6ac99df547e19ca9
alexzand <zengquanzhan@163.com>
2017-08-07 23:25:24 -0700

    first commit readme example.c

---
+++ example.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: example.c
+    > Author: alexzand
+    > Mail: zengquanzhan.com 
+    > Created Time: Mon 07 Aug 2017 11:23:18 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+	printf("hello,git\n");
+	return 0;
+}
